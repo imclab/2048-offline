0d900780796c4d08027ffbcd73acffc2a97f7856
gilsondev <contato@gilsondev.com>
2014-03-16 22:56:22 -0300

    Update gruntfile and generate icon

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,13 +4,14 @@ module.exports = function(grunt) {
     var gruntConfig = {
       nodewebkit: {
         options: {
-            build_dir: './webkitbuilds', // Where the build version of my node-webkit app is saved
+            build_dir: './build', // Where the build version of my node-webkit app is saved
+            mac_icns: 'icon.icns',
             mac: true, // We want to build it for mac
             win: true, // We want to build it for win
-            linux32: false, // We don't need linux32
+            linux32: true, // We don't need linux32
             linux64: false // We don't need linux64
         },
-        src: ['package.json']
+        src: './public/**/*'
       },
     };
  
